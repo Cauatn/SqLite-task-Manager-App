0e82eabb9ae36d45999e3dc95b19866e64f2f89d
Cauã Tavares <tavarescauac@gmail.com>
2023-12-31 13:29:48 -0300

    build: estilizando formulario de novos items

--- lib/main.dart
+++ lib/main.dart
@@ -57,21 +57,100 @@ class _MyHomePageState extends State<MyHomePage> {
           bottom: MediaQuery.of(context).viewInsets.bottom + 200,
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(
               height: 50,
             ),
-            TextFormField(controller: _titleController),
-            TextFormField(controller: _descriptionController),
-            ElevatedButton(
-              onPressed: () async {
-                await adicionarItem(
-                  _titleController.text,
-                  _descriptionController.text,
-                );
-              },
-              child: const Text('adicionar item'),
-            )
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Adicione um novo item!',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Me conte sobre oque fará nele :)',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 23,
+                    color: Colors.grey.shade500,
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+                right: 6,
+                left: 6,
+              ),
+              child: TextFormField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                    contentPadding:
+                        EdgeInsets.only(top: 15), // add padding to adjust text
+                    isDense: true,
+                    hintText: "Title",
+                    iconColor: Colors.grey,
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(
+                          top: 10), // add padding to adjust icon
+                      child: Icon(Icons.title_sharp),
+                    )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 20,
+                right: 6,
+                left: 6,
+              ),
+              child: TextFormField(
+                  controller: _descriptionController,
+                  decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.only(
+                        top: 15,
+                      ), // add padding to adjust text
+                      isDense: true,
+                      hintText: "Description",
+                      iconColor: Colors.grey,
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(
+                            top: 10), // add padding to adjust icon
+                        child: Icon(Icons.edit_document),
+                      ))),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              width: 150,
+              height: 45,
+              child: FloatingActionButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                backgroundColor: const Color.fromARGB(255, 164, 132, 250),
+                child: const Text(
+                  "Criar novo item",
+                  style: TextStyle(
+                    color: Color.fromARGB(245, 255, 255, 255),
+                  ),
+                ),
+                onPressed: () async {
+                  await adicionarItem(
+                    _titleController.text,
+                    _descriptionController.text,
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
